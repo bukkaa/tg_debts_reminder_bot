1653b03d473896b93f3e3d85148c1370f59bddc9
Denis Medvedev <bukkaa777@gmail.com>
2022-07-10 00:53:03 +0200

    more variants of application-local properties added to gitignore; java version downgraded to 11; renamed telegram properties in application.properties to be loaded by @ConfigurationProperties bean; main Application class renamed; Bot name as encoded string added to properties file

--- src/main/java/com/dreamteam/debtsreminderbot/DebtsReminderBotApplication.java
+++ src/main/java/com/dreamteam/debtsreminderbot/DebtsReminderBotApplication.java
@@ -1,23 +1,15 @@
 package com.dreamteam.debtsreminderbot;
 
 import com.dreamteam.debtsreminderbot.configuration.TelegramConfig;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
-import javax.annotation.PostConstruct;
-
-@Slf4j
 @SpringBootApplication
 @EnableConfigurationProperties(value = TelegramConfig.class)
-public class Application {
+public class DebtsReminderBotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(DebtsReminderBotApplication.class, args);
     }
-
 }
